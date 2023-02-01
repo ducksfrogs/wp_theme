afefd701d12bdd912d05b36dc22066b1f26a7dc6
ducksfrogs <ma_yamaki@yahoo.com>
2023-02-02 07:27:11 +0900

    d

--- wordpress/wp-content/themes/ppu-sky/index.php
+++ wordpress/wp-content/themes/ppu-sky/index.php
@@ -1,1 +1,10 @@
-This is our amazing custom theme. This
+<?php 
+    function greet($name, $color) {
+        echo "<p>Hi my name is $name. $color</p>";  # code...
+    }
+    
+    greet('Jone', 'Blue');
+    greet('Jame', 'green');
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
