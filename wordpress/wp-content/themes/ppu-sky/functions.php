a724a92276a604baa8f2c6a47402097b22921632
ducksfrogs <ma_yamaki@yahoo.com>
2023-02-04 16:14:31 +0900

    dd

--- wordpress/wp-content/themes/ppu-sky/functions.php
+++ wordpress/wp-content/themes/ppu-sky/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function univ_files() {
-    wp_enqueue_script( 'mainiscript', get_theme_file_uri('./build/index.js'), array('jquery') , '1.0', true);
+    wp_enqueue_script( 'maini-momo', get_theme_file_uri('./build/index.js'), array('jquery') , '1.0', true);
     wp_enqueue_style( 'font-awsom',  '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'  ) ;
     wp_enqueue_style( 'google-font',  '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i'  ) ;
     wp_enqueue_style( 'univ_main_style', get_theme_file_uri( './build/style-index.css' )  ) ;
