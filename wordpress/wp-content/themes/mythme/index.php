525ed3d0bead26e3d81fe608185f03e1db75f1d6
ducksfrogs <ma_yamaki@yahoo.com>
2023-05-07 06:14:34 +0900

    dd

--- wordpress/wp-content/themes/mythme/index.php
+++ wordpress/wp-content/themes/mythme/index.php
@@ -6,16 +6,13 @@ get_header(  );
 ?>
 
     <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg')?>)"></div>
-      <div class="page-banner__content container t-center c-white">
-        <h1 class="headline headline--large">Welcome to our blog</h1>
-        <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
-        <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-        <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
-      </div>
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri( '/images/ocean.jpg' ) ?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title">Welcome to our blog</h1>
         <div class="page-banner__intro">
         <p>Keep up with out lataset news.</p>
         </div>
+      </div>
     </div>
     
 <div class="container">
@@ -23,18 +20,22 @@ get_header(  );
   while(have_posts(  )){
     the_post(  ); ?>
   <div class="post-item">
-    <h2 class="headline headline--medium headline--post--title"><a href="<?php the_permalink(  ); ?>"><?php the_title(  ); ?></a></h2>
+    <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(  ); ?>"><?php the_title(  ); ?></a></h2>
     <div class="metabox">
-      <p>Posted by <?php the_author_posts_link(  ); ?> on 6.8.17 in News</p>
+      <p>Posted by <?php the_author_posts_link(  ); ?> on <?php the_time( 'n.j.y'); ?> in <?php echo get_the_category_list( ', ' ); ?>
+       </p>
     </div>
     <div class="generic-content">
       <?php the_excerpt(  ); ?>
-      <p><a class="btn btn--blue" href="<?php the_permalink(  ); ?>">conti</a></p>
+      <p><a class="btn btn--blue" href="<?php the_permalink(  ); ?>">continu reading</a></p>
     </div>
   </div>
 </div>
 <?php }
+  echo paginate_links(  );
+?>
 
+<?php
 get_footer( );
 
 ?>
