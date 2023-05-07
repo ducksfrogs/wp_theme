6a8a2c456aa64076c9999474b86f159b567d4eaa
ducksfrogs <ma_yamaki@yahoo.com>
2023-05-07 15:24:41 +0900

    dd

--- wordpress/wp-content/themes/mythme/front-page.php
+++ wordpress/wp-content/themes/mythme/front-page.php
@@ -14,7 +14,6 @@
       <div class="full-width-split__one">
         <div class="full-width-split__inner">
           <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
-
           <div class="event-summary">
             <a class="event-summary__date t-center" href="#">
               <span class="event-summary__month">Mar</span>
@@ -43,6 +42,26 @@
         <div class="full-width-split__inner">
           <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
 
+           <?php 
+          $homepagePosts = new WP_Query(array(
+            'posts_per_page' => 1,
+          ));
+
+          while($homepagePosts->have_posts()){
+            $homepagePosts->the_post();  ?>
+              <div class="event-summary">
+                <a class="event-summary__date event-summary__date--beige t-center" href="#">
+                  <span class="event-summary__month"><?php the_time('M'); ?></span>
+                  <span class="event-summary__day"><?php the_time('d'); ?></span>
+                </a>
+                <div class="event-summary__content">
+                  <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(  ); ?>"><?php the_title(  ); ?></a></h5>
+                  <p><?php echo wp_trim_words(get_the_content(  ), 18); ?> <a href="<?php the_permalink(  ); ?>" class="nu gray">Read more</a></p>
+                </div>
+              </div>
+            <?php
+          }
+          ?> 
           <div class="event-summary">
             <a class="event-summary__date event-summary__date--beige t-center" href="#">
               <span class="event-summary__month">Jan</span>
